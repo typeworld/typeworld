89e896cbc9b32dcb5988320ad2ea54fd6f3ca01c
Yanone <post@yanone.de>
2018-01-14 12:39:50 +0200

    Moved code around

---
+++ Lib/typeWorld/client/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+import os, sys, json
+
+from typeWorld.classes import *
+import typeWorld.api
+
+
+
+
+class Repository(object):
+	u"""\
+	Represents a font repository under a specific API endpoint.
+	"""
+
+class APIEndPoint(object):
+	u"""\
+	Represents an API endpoint, identified and grouped by the canonical URL attribute of the API responses. This API endpoint class can then hold several repositories.
+	"""
+
+
+class APIClient(object):
+	u"""\
+	Main Type.World client app object. Use it to load repositories and install/uninstall fonts.
+	"""
+
+	def __init__(self):
+
+		self.endpoints = []
+		
