1ffdf63981d04c060addd95d1e82d723d2a8c23d
Yanone <post@yanone.de>
2019-10-08 18:57:28 +0200

    Moved

--- Lib/typeWorld/api/base.py
+++ Lib/typeWorld/api/base.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import json, copy, types, inspect, re, traceback, datetime, lxml.html, markdown
-import semver
+import json, copy, types, inspect, re, traceback, datetime, lxml.html, markdown, semver
 
 import typeWorld.api
 import typeWorld.api.base
