0aae6f365a6626558b406537add7b4dfe70a5550
Yanone <post@yanone.de>
2019-11-06 13:17:36 +0100

    Tried to execute this properly, let's see

--- Lib/typeWorld/api/__init__.py
+++ Lib/typeWorld/api/__init__.py
@@ -441,8 +441,8 @@ class BaseResponse(DictBasedObject):
 	def customValidation(self):
 		information, warnings, critical = [], [], []
 
-		if self.type == ERROR and self.errorMessage is None:
-			critical.append('%s.type is "%s", but .errorMessage is missing.' % (self, ERROR))
+		if self.type == ERROR and self.errorMessage.isEmpty():
+			critical.append('%s.type is "%s", but %s.errorMessage is missing.' % (self, ERROR, self))
 
 		return information, warnings, critical
 
