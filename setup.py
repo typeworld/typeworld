9e3aba53e1e5ebd862b78b034bf66fe6ab36e677
Yanone <post@yanone.de>
2020-11-23 14:34:24 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if MAC:
 
 setup(
     name="typeworld",  # How you named your package folder (MyLib)
-    version="0.2.3b7",
+    version="0.2.3b8",
     license="apache-2.0",
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="""This module hosts the Type.World JSON Protocol definitions in
