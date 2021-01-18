4111ffd4b8f0fc31fb3e2d0342254047d38b0430
Yanone <post@yanone.de>
2021-01-18 18:00:22 +0100

    Simplified Execute() method

--- Lib/typeworld/api/makeDocu.py
+++ Lib/typeworld/api/makeDocu.py
@@ -9,28 +9,17 @@ def Execute(command):
     Execute system command, return output.
     """
 
-    import sys
     import os
-    import platform
+    import subprocess
 
-    if sys.version.startswith("2.3") or platform.system() == "Windows":
-
-        p = os.popen(command, "r")
-        response = p.read()
-        p.close()
-        return response
-
-    else:
-
-        import subprocess
-
-        process = subprocess.Popen(
-            command, stdout=subprocess.PIPE, shell=True, close_fds=True
-        )
-        os.waitpid(process.pid, 0)
-        response = process.stdout.read().strip()
-        process.stdout.close()
-        return response
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, shell=True, close_fds=True
+    )
+    os.waitpid(process.pid, 0)
+    response = process.stdout.read().strip()
+    process.stdout.close()
+    process.wait()
+    return response
 
 
 docstrings = []
