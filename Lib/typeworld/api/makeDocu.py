2c59cca6005f636a0a4c9656597d7455f5017cc3
Yanone <post@yanone.de>
2021-02-03 15:42:03 +0100

    auto-update docs

--- Lib/typeworld/api/makeDocu.py
+++ Lib/typeworld/api/makeDocu.py
@@ -2,13 +2,16 @@
 
 import os
 import sys
+import site
 
 # Use local code for local testing, and rely on system-installed module for CI-testing
 CI = os.getenv("CI", "false").lower() != "false"
 if not CI:
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
     if path not in sys.path:
-        sys.path.append(path)
+        sys.path.insert(0, path)
+    if site.getsitepackages() not in path:
+        sys.path.insert(0, site.getsitepackages())
 
 import typeworld.api  # noqa: E402
 
